141665f00cfd343a2171d457052c6184cb9b36ed
Dm Mov <movchanyukd@gmail.com>
2019-07-13 01:18:49 +0300

    change todo model

--- Models/Todo.cs
+++ Models/Todo.cs
@@ -1,14 +1,13 @@
-﻿using System;
+﻿using my_own_json_api.Models.UIModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace my_own_json_api.Models
 {
-    public class Todo
+    public class Todo : TodoUI
     {
         public string Id { get; set; }
-        public string Title { get; set; }
-        public bool Completed { get; set; }
     }
 }
